0c1fa3d8e989b8a3c677972f8ca2f1ee01977e63
Deborah Pelacani Cruz <dekape23@gmail.com>
2023-06-08 07:47:46 +0000

    comments

--- train.py
+++ train.py
@@ -3,6 +3,16 @@ from utils.setup import setup_train
 from runners.trainer import Trainer
 
 if __name__ == "__main__":
+    # Gather arguments from CL and from YAML config file,
+    # adjust and check for arguemnts
+    # create necessary folders for experiment
     args = setup_train()
+
+    # With given args, set up the training model with
+    # datasets, dataloaders, model and optimiser instan
+    # ciations, and data conditioning
     trainer = Trainer(args)
+
+    # Train autoencoder (fisrt stage model) and diffusion
+    # in sequence
     trainer.train()
