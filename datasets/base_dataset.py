d1dfb4acc5b61850ad930083fce27d513cda0209
Deborah Pelacani Cruz <dekape23@gmail.com>
2023-07-25 15:39:59 +0000

    add condition to datasets

--- datasets/base_dataset.py
+++ datasets/base_dataset.py
@@ -3,13 +3,14 @@ from abc import abstractmethod
 from torch.utils.data import Dataset
 from torchvision.transforms import Compose, Resize, Lambda, ToTensor, Normalize
 
-from utils.utils import scale2range, clip_outliers
+from utils.utils import scale2range, clip_outliers, namespace2dict
 
 
 class BaseDataset(Dataset):
     def __init__(self, args):
         self.args = args
-        self.transform = self._get_transform()
+        self.transform = self._get_transform(**namespace2dict(self.args))
+        self.cond_transform = self._get_transform(**namespace2dict(self.args.condition))
 
     @abstractmethod
     def __len__(self):
@@ -19,22 +20,28 @@ class BaseDataset(Dataset):
     def __getitem__(self, index):
         raise NotImplementedError
 
-    def _get_transform(self):
+    def _get_transform(self, **kwargs):
+        resize = kwargs.pop("resize", None)
+        to_tensor = kwargs.pop("to_tensor", True)
+        outliers = kwargs.pop("clip_outliers", False)
+        scale = kwargs.pop("scale", [0, 1])
+        normalise = kwargs.pop("normalise", False)
+
         transform_list = []
-        if self.args.image_size:
-            transform_list.append(Resize(self.args.image_size, antialias=True))
-        if self.args.to_tensor:
+        if resize:
+            transform_list.append(Resize(resize, antialias=True))
+        if to_tensor:
             transform_list.append(ToTensor())
-        if self.args.clip_outliers:
+        if outliers:
             transform_list.append(
-                lambda x: clip_outliers(x, self.args.clip_outliers)
+                lambda x: clip_outliers(x, outliers)
             )
-        if self.args.scale:
-            scale = self.args.scale
+        if scale:
+            scale = scale
             transform_list.append(Lambda(
                 lambda x: scale2range(x, [scale[0], scale[-1]])))
-        if self.args.normalise:
-            transform_list.append(Normalize(self.args.normalise[0],
-                                            self.args.normalise[1]))
+        if normalise:
+            transform_list.append(Normalize(normalise[0],
+                                            normalise[1]))
 
         return Compose(transform_list)
--- datasets/brain2d_dataset.py
+++ datasets/brain2d_dataset.py
@@ -3,12 +3,18 @@ import gzip
 import numpy as np
 import torch
 from . import BaseDataset
+
+
 class Brain2DDataset(BaseDataset):
     def __init__(self, args):
         BaseDataset.__init__(self, args)
+
+        # Save args in object
+        self.args = args
+
         # Checking mode
         try:
-            mode = args.mode
+            mode = self.args.mode
             if mode.lower() in ["mri", "vp"]:
                 self.mode = mode
             else:
@@ -19,13 +25,13 @@ class Brain2DDataset(BaseDataset):
 
         # Check maxsamples
         try:
-            maxsamples = args.maxsamples
+            maxsamples = self.args.maxsamples
         except KeyError:
             maxsamples = None
 
         # Get image paths
         self.data_paths = []
-        self._get_image_paths(args.data_path, maxsamples)
+        self._get_image_paths(self.args.data_path, maxsamples)
         return None
 
     def _get_image_paths(self, path, maxsamples=None):
@@ -41,25 +47,59 @@ class Brain2DDataset(BaseDataset):
         self.data_paths = self.data_paths[:maxsamples]
         assert len(self.data_paths) > 0, " Found no data samples to load"
         return None
+    
+    def _get_condition_path(self, data_path):
+        """
+        Finds inside self.args.condition.path a .npy or .npy.gz file
+        containing the name of the data item of data_path
+        """
 
-    def __getitem__(self, index):
-        # Item path
-        y_path = self.data_paths[index]
+        name = os.path.splitext(os.path.split(data_path)[-1])[0]
 
-        # Read data
-        if y_path.endswith(".npy.gz"):
-            with gzip.open(y_path, 'rb') as f:
+        cond_path = os.path.join(self.args.condition.path, name + f"-{self.args.condition.mode}")
+
+        if os.path.isfile(cond_path + ".npy"):
+            return cond_path + ".npy"
+        elif os.path.isfile(cond_path + ".npy.gz"):
+            return cond_path + ".npy.gz"
+        else:
+            raise FileNotFoundError(f"Condition file {cond_path} does not exist in neither .npy or .npy.gz format")
+
+    
+    def _read_npy_data(self, data_path):
+        # Load .npy or .npy.gz data into torch tensor
+        if data_path.endswith(".npy.gz"):
+            with gzip.open(data_path, 'rb') as f:
                 y = torch.from_numpy(np.load(f))
-        elif y_path.endswith(".npy"):
-            y = torch.from_numpy(np.load(y_path))
+        elif data_path.endswith(".npy"):
+            y = torch.from_numpy(np.load(data_path))
         else:
             raise TypeError
 
+        # Check channel dimension exists
+        if len(y.shape) < 3:
+            y = y.unsqueeze(0)
+        return y
+
+    def __getitem__(self, index):
+        # Data item path
+        y_path = self.data_paths[index]
+
+        # Read data
+        y = self._read_npy_data(y_path)
+        
         # Apply transform
         if self.transform:
             y = self.transform(y)
 
-        return y, 0
+        # Get condition, apply steps above
+        if self.args.condition.mode is not None and self.args.condition.path is not None:
+            cond_path = self._get_condition_path(y_path)
+            cond = self._read_npy_data(cond_path)
+            if self.cond_transform:
+                cond = self.cond_transform(cond)
+            return y, cond
+        return y
 
     def __len__(self):
         return len(self.data_paths)
