6a89b6e0108f4ded9fc80d246a6b90adc470a880
Deborah Pelacani Cruz <deborah.pelacani@gmail.com>
2023-12-07 11:22:21 +0000

    no bias regularisation

--- models/utils.py
+++ models/utils.py
@@ -30,8 +30,21 @@ def _instance_optimiser(args, model):
         except KeyError:
             pass
 
+    # change weight decay of biases to zero if prompted
+    if args.optim.bias_weight_decay is not None:
+        decay_params, no_decay_params = [], []
+        for name, param in model.named_parameters():
+            if not param.requires_grad:
+                continue           
+            elif len(param.shape) == 1 or name.endswith(".bias"):
+                no_decay_params.append(param)
+            else: decay_params.append(param)
+        params = [{'params': no_decay_params, 'weight_decay': args.optim.bias_weight_decay}, {'params': decay_params, 'weight_decay': kwargs.pop("weight_decay")}]        
+    else:
+        params=model.parameters()
+
     # instance optimiser
-    optimiser = cls(model.parameters(), **kwargs)
+    optimiser = cls(params, **kwargs)
     return optimiser
 
 
