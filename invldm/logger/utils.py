02373ea17ab30c87fdeb25d9eccc448f1400b1ec
dekape <deborah.pelacani@gmail.com>
2024-04-15 12:16:54 +0100

    add msg of case sensitivity to logger error

--- invldm/logger/utils.py
+++ invldm/logger/utils.py
@@ -15,7 +15,7 @@ def _instance_logger(args):
             cls = getattr(mod, cls_name)
         except AttributeError as e:
             logging.warn(f'Expecting {args.logging.tool.lower()}_logger.py'
-                        f' to contain a logger class named {cls_name}')
+                        f' to contain a logger class named {cls_name}. Note that this is case sensitive.')
             raise (AttributeError, e)
         assert (issubclass(cls, BaseLogger))
         logger = cls(args)
