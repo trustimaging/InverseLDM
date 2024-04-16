4b04531e705972f4fef1adc8b4951a9491105a2a
dekape <deborah.pelacani@gmail.com>
2024-04-16 09:55:58 +0100

    syntax correction on exception raise

--- invldm/loggers/utils.py
+++ invldm/loggers/utils.py
@@ -16,7 +16,7 @@ def _instance_logger(args):
         except AttributeError as e:
             logging.warn(f'Expecting {args.logging.tool.lower()}_logger.py'
                         f' to contain a logger class named {cls_name}. Note that this is case sensitive.')
-            raise (AttributeError, e)
+            raise AttributeError(e)
         assert (issubclass(cls, BaseLogger))
         logger = cls(args)
     else:
