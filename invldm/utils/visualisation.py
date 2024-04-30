d3b9dcc28fd22e8dc45ad1026cb8fadc663d9108
dekape <deborah.pelacani@gmail.com>
2024-04-30 21:20:26 +0100

    add oscar's cmap

--- invldm/utils/visualisation.py
+++ invldm/utils/visualisation.py
@@ -1,6 +1,15 @@
+import os
+import pickle
 import matplotlib.pyplot as plt
+from matplotlib.colors import LinearSegmentedColormap
+
 from torchvision.utils import make_grid
+
 from .utils import scale2range
+
+
+with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), "oscar_cmap.pkl"), "rb") as f:  
+    OSCAR_CMAP = LinearSegmentedColormap.from_list("oscar_cmap", pickle.load(f))
 
 
 def visualise_samples(x, scale=False):
@@ -10,5 +19,7 @@ def visualise_samples(x, scale=False):
         x = scale2range(x, [0, 1])
     fig = plt.figure()
     grid = make_grid(x).permute(1, 2, 0).cpu()
-    plt.imshow(grid)
+    if x.shape[1] == 1:
+        grid = grid[:, :, 0]
+    plt.imshow(grid, cmap=OSCAR_CMAP)
     return fig
