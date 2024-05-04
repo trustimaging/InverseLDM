d6a796b6d4e62a0885e4a7137cd9fcf9915e3e0f
dekape <deborah.pelacani@gmail.com>
2024-05-04 10:48:56 +0100

    i regret my change from before

--- invldm/datasets/base_dataset.py
+++ invldm/datasets/base_dataset.py
@@ -30,9 +30,6 @@ class BaseDataset(Dataset):
         normalise = kwargs.pop("normalise", False)
 
         transform_list = []
-        if normalise:
-            transform_list.append(Normalize(normalise[0],
-                                            normalise[1]))
         if resize:
             transform_list.append(Resize(resize, antialias=antialias))
         if to_tensor:
@@ -42,6 +39,9 @@ class BaseDataset(Dataset):
         if scale:
             scale = scale
             transform_list.append(Scale(scale=scale))
+        if normalise:
+            transform_list.append(Normalize(normalise[0],
+                                            normalise[1]))
 
         return Compose(transform_list)
     
